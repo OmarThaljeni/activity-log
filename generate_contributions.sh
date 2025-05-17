e9fa50c4ecfa738323694351dc287b01f7c73fbe
Thaljeni Omar <61472797+OmarThaljeni@users.noreply.github.com>
2025-05-17 13:46:09 +0100

    Update generate_contributions.sh

--- generate_contributions.sh
+++ generate_contributions.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-echo "Dummy commit $(date)" > commit.txt
-git add commit.txt
-git commit -m "Automated commit $(date)"
+# Number of commits between 10 and 40 (random)
+NUM_COMMITS=$(( RANDOM % 31 + 10 ))
+
+FILE="commit.txt"
+
+for ((i=1; i<=NUM_COMMITS; i++))
+do
+  echo "Commit #$i at $(date)" >> $FILE
+  git add $FILE
+  git commit -m "Automated commit #$i at $(date)" || echo "No changes to commit"
+done
